b38576ff0041ad90f10585f3c50898bd0c7ed5c1
tv3636 <tv3636@gmail.com>
2022-10-19 16:36:20 -0700

    update share image

---
+++ components/SiteHead.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import React from "react";
+
+export default function SiteHead() {
+  let title = 'Forgotten Runes Wizard\'s Cult Burn Log';
+  let image = "https://burnlog.vercel.app/new-tulip.png";
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta name="twitter:title" content={title} key="twittertitle" />
+      <meta name="twitter:card" content="summary_large_image" key="twcard" />
+
+      <meta name="twitter:image" content={image} key="twimage"/>
+      <meta property="og:image" content={image} key="ogimage"/>
+    </Head>
+  )
+}
--- pages/flame-shame.js
+++ pages/flame-shame.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import OwnerRow from "../components/OwnerRow";
+import SiteHead from "../components/SiteHead";
 
 function MainView({ data }) {
   if (!data || !data.owners) {
@@ -59,15 +59,7 @@ export default function Home() {
         </div>
       ) : (
         <div className={styles.container}>
-          <Head>
-            <title>Burn Log</title>
-            <meta
-              name="description"
-              content="Forgotten Runes Wizard's Cult Burn Log"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-
+        <SiteHead/>
           <h1>Forgotten Runes Wizard&apos;s Cult Flame Shame</h1>
           <h3>Burn the wizards!</h3>
           <div
@@ -109,7 +101,7 @@ export default function Home() {
         }}
       >
         <a
-          href="https://twitter.com/tv3636"
+          href="https://twitter.com/thecodingadvent"
           target="_blank"
           rel="noopener noreferrer"
           style={{ margin: "10px" }}
